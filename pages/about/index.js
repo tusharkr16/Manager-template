8223f35f93a0b339bb54e0f546c1bf7997cb98e3
tusharkr16 <tusharkumar9871@gmail.com>
2021-05-28 22:51:01 +0530

    added side navbar to home page

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,13 +1,6 @@
-console.log("JavaScript Loaded")
-
-const onGetStarted = () => {
-    window.open("./pages/home/index.html", "_self");
-};
-let openNav=()=>{
-
+let openNav = () => {
     document.getElementById("mySidenav").style.width = "250px";
-}
-  
+  }
   
   let closeNav=() => {
     document.getElementById("mySidenav").style.width = "0";
